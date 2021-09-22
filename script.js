c08ed57199c262a7fb36290f8b78a9d39097b43d
Guilherme Datilio Ribeiro1 <guilherme.datilio@ibm.com>
2021-09-22 00:38:50 -0300

    Fetching the Pokemon and displaying in the HTML

--- script.js
+++ script.js
@@ -1,12 +1,29 @@
+document.querySelector("#search").addEventListener("click", getPokemon);
+
 function getPokemon(e) {
-  fetch(`https://pokeapi.co/api/v2/pokemon/ditto`)
+  const name = document.querySelector("#pokemonName").value;
+
+  fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
     .then((response) => response.json())
     .then((data) => {
-      console.log(data);
+      document.querySelector(".pokemonBox").innerHTML = `
+      <div>
+        <img
+          src="${data.sprites.other["official-artwork"].front_default}"
+          alt="Pokemon name"
+        />
+      </div>
+      <div class="pokemonInfos">
+        <h1>${data.name}</h3>
+        <p>Weight: ${data.weight}</p>
+      </div>`;
     })
     .catch((err) => {
+      document.querySelector(".pokemonBox").innerHTML = `
+      <h4>Pokemon not found 😞</h4>
+      `;
       console.log("Pokemon not found", err);
     });
-}
 
-getPokemon();
+  e.preventDefault();
+}
